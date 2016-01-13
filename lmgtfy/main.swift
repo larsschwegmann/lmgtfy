7caf4788c5c302ddaf2c1d2d2ed89aa0edabe850
Lars Schwegmann <schwegmannlars@gmail.com>
2016-01-13 17:05:43 +0100

    Initial Commit

---
+++ lmgtfy/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lmgtfy
+//
+//  Created by Lars on 13.01.16.
+//  Copyright © 2016 larsschwegmann.com. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
